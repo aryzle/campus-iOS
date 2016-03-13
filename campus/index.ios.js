99ba7a79587a90cb9f3fef3461d78466518071a3
aryzle <seghaa@rpi.edu>
2016-03-13 14:58:15 -0400

    setting up geo fencing

---
+++ campus/index.ios.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import bgGeo from 'react-native-background-geolocation';
+
+bgGeo.addGeofence({
+  identifier: "Home",
+  radious: 200,
+  latitude: 35.912343,
+  longtitude: -79.069016,
+  notifyOnEntry: true
+});
+
+class campus extends Component {
+
+  constructor() {
+    super();
+    let me = this;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('campus', () => campus);
